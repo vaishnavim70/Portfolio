849e779a636c37ae7b317ce67c579f703dd5fd55
vaishnavim70 <vaishnavimugale70@gmail.com>
2024-10-14 18:56:32 +0530

    First Commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+AOS.init();
+
+//header scroll
+
+let nav = document.querySelector(".navbar")
+window.onscroll = function(){
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("header-scrooled");
+    }else{
+        nav.classList.remove("header-scrooled");
+    }
+}
+
+//nav hide
+let navBar = document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function(a){
+    a.addEventListener("click", function(){
+        navCollapse.classList.remove("show");
+    })
+})
+
+
